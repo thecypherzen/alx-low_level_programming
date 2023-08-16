a0f39f1a929f126a0e0c9fd366a92ebb195f504e
your_name <your_email>
2023-08-16 04:22:27 +0000

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
