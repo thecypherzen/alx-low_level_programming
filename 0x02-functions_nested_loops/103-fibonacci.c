162e05cd899cb0bdb20a0908abc4136b329c8152
your_name <your_email>
2023-08-16 14:53:20 +0000

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the sum of the even-valued fibonacci
+ * terms <= 4 000 000
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	long int start;
+	long int next;
+	long int temp;
+	int cur_term;
+	long int sum;
+
+	start = 1;
+	next = 2;
+	cur_term = 2;
+	sum = 0;
+
+	while (next <= 4000000)
+	{
+		if (cur_term % 2 == 0)
+			sum += next;
+		temp = start + next;
+		start = next;
+		next = temp;
+		cur_term++;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
