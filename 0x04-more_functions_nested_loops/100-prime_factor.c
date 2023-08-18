961ac9d2ea1895dd50ad59eaafcfdc15e8fbedde
William Inyam <sw.inyam@outlook.com>
2023-08-18 02:00:13 +0000

    main.h 100-prime_factor.c update

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,24 +7,17 @@
  */
 int main(void)
 {
-	unsigned long int num, temp, count, sqrp;
+	long int num, count;
 
 	num = 612852475143;
-	temp = num;
-	sqrp = sqrt(num);
 
-	/* if temp is even, divide by 2 until odd number is obtained */
-	while (!((temp % 10) % 2))
-		temp /= 2;
-
-	/* start counting from 3 and checking for odd primes */
-	for (count = 3; count <= sqrp; count += 2)
+	for (count = 3; count != num; count += 2)
 	{
-		while (!(temp % count))
-			temp /= count;
+		while (!(num % count))
+			num /= count;
 	}
 
 	/* print final value of temp (highest prime factor) */
-	printf("%lu\n", temp);
+	printf("%lu\n", num);
 	return (0);
 }
