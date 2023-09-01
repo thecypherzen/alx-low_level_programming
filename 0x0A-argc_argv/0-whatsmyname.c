2b8951be513f6528604295f5048a59c8fd9c24e7
William Inyam <sw.inyam@outlook.com>
2023-09-01 15:10:14 +0000

    Submit 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+ * main - Entry point
+ * @argsc: cmd argument count
+ * @argsv: cmd argument list
+ * Return: 0 always
+ */
+int main(int argsc, char **argsv)
+{
+	printf("%s\n", *argsv);
+	(void)argsc;
+	return (0);
+}
