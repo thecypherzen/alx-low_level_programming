6644d0ae2d93f2c185f11f5b1e23f9d7729f165f
William Inyam <sw.inyam@outlook.com>
2023-09-02 05:52:24 +0000

    Update 100-change.c x4

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,56 +8,24 @@
  */
 int main(int argsc, char **argsv)
 {
-	int idx;
-	char *curr;
+	int idx, num, coins;
+	int arr[] = {25, 10, 5, 2, 1};
 
 	if (argsc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	curr = argsv[1];
-	if (curr[0] == '-')
-	{
-		printf("%d\n", 0);
-		return (0);
-	}
-	idx = 0;
-	while (curr[idx] != '\0')
-	{
-		if (curr[idx] < 48 || curr[idx] > 57)
-		{
-			/*printf("%d\n", 0);*/
-			return (0);
-		}
-		idx++;
-	}
-
-	coins_cal(atoi(curr));
-	return (0);
-}
-
-/**
- * coins_cal - calculates the total number of coins and prints
- * @num: the number to calculate coins of
- * Return: void
- */
-void coins_cal(int num)
-{
-	int coins, idx;
-	int arr[] = {25, 10, 5, 2, 1};
-
+	num = atoi(argsv[1]);
 	coins = 0;
-
 	for (idx = 0; idx < 5 && num > 0; idx++)
 	{
-		if (num >= arr[idx])
+		while (num >= arr[idx])
 		{
 			num -= arr[idx];
 			coins++;
 		}
-		else
-			continue;
 	}
 	printf("%d\n", coins);
+	return (0);
 }
