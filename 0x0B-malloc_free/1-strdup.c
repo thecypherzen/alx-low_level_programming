aa5806ac80edfe2ac07cef173b049c89fa616935
William Inyam <sw.inyam@outlook.com>
2023-09-04 12:40:12 +0000

    Submit 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strdup - copies a string to another location in memory
+ * @str: the string
+ * Return: pointer to string on success. NULL otherwise.
+ */
+char *_strdup(char *str)
+{
+	char *newstr;
+	int i, sz;
+
+	sz = (int)strlen(str) + 1;
+	newstr = malloc(sz * sizeof(char));
+	if (newstr != NULL)
+	{
+		for (i = 0; i < sz; i++)
+			*(newstr + i) = *(str + i);
+	}
+	return (newstr);
+}
