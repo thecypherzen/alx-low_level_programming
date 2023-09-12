4d04d03eb8ed62ec0cc8d2027683f524976779cb
William Inyam <sw.inyam@outlook.com>
2023-09-12 17:05:43 +0000

    4-new_dog.c update 1

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,24 +14,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog = malloc(sizeof(dog_t));
 	if (!dog || !owner || !name)
+	{
+		free(dog);
 		return (NULL);
-
-	len_name = (int)strlen(name);
-	len_own = (int)strlen(owner);
+	}
+	len_name = (int)strlen(name), len_own = (int)strlen(owner);
 	dog->name = malloc(sizeof(char) * (len_name + 1));
 	dog->owner = malloc(sizeof(char) * (len_own + 1));
-
 	if (!dog->name || !dog->owner)
 	{
 		free(dog->name), free(dog->owner), free(dog);
 		return (NULL);
 	}
-
-	for (k = 0; k < len_name; k++)
-		*(dog->name + k) = *(name + k), *(dog->name + k) = '\0';
-	for (k = 0; k < len_own; k++)
-		*(dog->owner + k) = *(owner + k), *(dog->owner + k) = '\0';
+	for (k = 0; k <= len_name; k++)
+		*(dog->name + k) = *(name + k);
+	for (k = 0; k <= len_own; k++)
+		*(dog->owner + k) = *(owner + k);
 	dog->age = age;
-
 	return (dog);
 }
