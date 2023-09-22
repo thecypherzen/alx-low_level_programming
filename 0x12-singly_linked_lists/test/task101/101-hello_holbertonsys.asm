efa4182de06e8a963a6d0d48aca7f3b0adb77e57
William Inyam <sw.inyam@outlook.com>
2023-09-22 09:43:30 +0000

    101-hello_holberton.asm submission

---
+++ 0x12-singly_linked_lists/test/task101/101-hello_holbertonsys.asm
@@ -0,0 +1,17 @@
+	;; prints a message to stdout using syscall
+
+	section .data 		;data section start
+msg:	db "Hello, Holberton", 10, 10
+
+	section .text 		;text section start
+	global main
+main:
+	mov rax, 0x01
+	mov rdi, 0x01
+	mov rsi, msg
+	mov rdx, 17
+	syscall
+
+	mov rax, 0x3c
+	mov rdi, 0x00
+	syscall
