7f8d5e5b48444e0192fe61d2eba2f33255276de0
William Inyam <sw.inyam@outlook.com>
2023-10-02 12:03:23 +0000

    Task 1 submission

--- 0x15-file_io/test/task1/main.h
+++ 0x15-file_io/test/task1/main.h
@@ -11,5 +11,5 @@ typedef unsigned long int _ulint;
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int _strlen(char *str);
+size_t _strlen(char *str);
 #endif
