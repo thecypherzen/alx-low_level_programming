94ee30412dd724d571dd9103f8766a1e4464751d
William Inyam <sw.inyam@outlook.com>
2023-10-02 12:44:35 +0000

    Task 2 submission

---
+++ 0x15-file_io/test/task2/2-append_text_to_file.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at end of a file
+ * - The created file must have those permissions: rw-------.
+ * - If the file already exists, do not change the permissions.
+ * - If the file already exists, truncate it
+ * - If text_content is NULL, add nothing to file
+ * @filename: name of file to create
+ * @text_content: a NULL terminated string to write to the file
+ * Return: 1 on success, - 1 on failure (file can not be created, file can not
+ *         be written, write “fails”, filename is NULL, etc…)
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	ssize_t fdes, bytes_wtn;
+
+	if (!filename)
+		return (-1);
+	fdes = open(filename, O_WRONLY | O_APPEND);
+	if (fdes < 0)
+		return (-1);
+	if (!text_content)
+	{
+		close(fdes);
+		return (1);
+	}
+	bytes_wtn = write(fdes, text_content, _strlen(text_content));
+	close(fdes);
+	if (bytes_wtn < 0)
+		return (-1);
+	return (1);
+}
+/**
+ * _strlen - gets the length of a null terminated string.
+ * @str: the string
+ * Return: len of str always.
+ */
+size_t _strlen(char *str)
+{
+	size_t len = 0;
+
+	if (str)
+	{
+		while (*(str++))
+			len++;
+	}
+	return (len);
+}
