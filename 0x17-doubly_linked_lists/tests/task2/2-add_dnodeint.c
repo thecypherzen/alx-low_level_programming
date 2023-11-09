1baca2676f8ffae2f7ddb3fef10fcde556344211
William Inyam <sw.inyam@outlook.com>
2023-11-09 13:32:51 +0000

    Task 2 update 2

--- 0x17-doubly_linked_lists/tests/task2/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/tests/task2/2-add_dnodeint.c
@@ -10,12 +10,24 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = NULL;
 
+
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
-		return (new_node);
+		return (NULL);
 	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = !(*head) ? NULL : *head;
+	if (!(*head) || !(*head)->prev)
+	{
+		new_node->next = *head;
+		if (*head)
+			(*head)->prev = new_node;
+		new_node->prev = NULL;
+	}
+	else
+	{
+		new_node->prev = *head;
+		(*head)->next = new_node;
+		new_node->next = NULL;
+	}
 	*head = new_node;
-	return(new_node);
+	return (new_node);
 }
