840555a330554b9f24c45f1da73bc902473f06c2
William Inyam <sw.inyam@outlook.com>
2023-12-01 18:29:50 +0000

    Task 4 submission

--- 0x1A-hash_tables/tests/test3/main3.c
+++ 0x1A-hash_tables/tests/test3/main3.c
@@ -3,50 +3,57 @@
 int main(void)
 {
 	hash_table_t *table = NULL;
-	ul_int size = 3;
+	ul_int size = 1024;
 	char *key, *value;
+	int ret;
 
 	table = hash_table_create(size);
 	if (table)
 	{
 		printf("hash table created successfully\n");
 
-		key = "home", value = "house";
-		hash_table_set(table, (const char *)key, (const char *)value);
-		print_hash(table);
-		key = "home", value = "them";
-		hash_table_set(table, (const char *)key, (const char *)value);
+		key = "hetairas", value = "house";
+		ret = hash_table_set(table, (const char *)key, (const char *)value);
+		printf("return: %d\n", ret);
 
-		/*key = "me", value = "us";
+		key = "mentioner", value = "them";
 		hash_table_set(table, (const char *)key, (const char *)value);
 
-		key = "gray", value = "deeer";
+		key = "heliotropes", value = "us";
 		hash_table_set(table, (const char *)key, (const char *)value);
 
-		key = "plumber", value = "peter";
+		key = "neurospora", value = "deeer";
 		hash_table_set(table, (const char *)key, (const char *)value);
 
-		key = "Driver", value = "Jackson";
+		key = "depravement", value = "peter";
 		hash_table_set(table, (const char *)key, (const char *)value);
 
-		key = "Main chef", value = "Douglas";
+		key = "serafins", value = "Jackson";
 		hash_table_set(table, (const char *)key, (const char *)value);
 
-		key = "Assistant Chef", value = "Madammeis";
+		key = "subgenera", value = "Douglas";
 		hash_table_set(table, (const char *)key, (const char *)value);
 
-		key = "Personal_Assistant", value = "Bori Minyo";
+		key = "stylist", value = "Madammeis";
 		hash_table_set(table, (const char *)key, (const char *)value);
 
-		key = "House_Keeper", value = "Polaris Arnold";
+		key = "joyful", value = "Bori Minyo";
 		hash_table_set(table, (const char *)key, (const char *)value);
 
-
-		key = "Security Guard", value = "Alexis Ikponko";
+		key = "synaphea", value = "Polaris Arnold";
 		hash_table_set(table, (const char *)key, (const char *)value);
 
-		key = "this", value = "there";
-		hash_table_set(table, (const char *)key, (const char *)value); */
+		key = "redescribed", value = "Alexis Ikponko";
+		hash_table_set(table, (const char *)key, (const char *)value);
+
+		key = "urites", value = "there";
+		hash_table_set(table, (const char *)key, (const char *)value);
+
+		key = "dram", value = "there";
+		hash_table_set(table, (const char *)key, (const char *)value);
+
+		key = "vivency", value = "there";
+		hash_table_set(table, (const char *)key, (const char *)value);
 
 		print_hash(table);
 	}
---
+++ 0x1A-hash_tables/tests/test4/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - fetches a value associated with a key
+ * @ht: ptr to hash table
+ * @key: the key to fetch
+ * Return: the key's value if found, else NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	ul_int index;
+	hash_node_t *temp;
+
+	if (!ht || !key || !strlen(key))
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+
+	temp = ht->array[index];
+	while (temp)
+	{
+		if (!strcmp(temp->key, key))
+			return temp->value;
+		temp = temp->next;
+	}
+	return (NULL);
+}
