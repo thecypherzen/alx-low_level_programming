68b3f1fbbef85d23eafe824e0dda5ce4caed869d
William Inyam <sw.inyam@outlook.com>
2024-07-16 20:18:34 +0000

    tests(allp:0x1E:11): test task

--- 0x1E-search_algorithms/tests/task11/main104.c
+++ 0x1E-search_algorithms/tests/task11/main104.c
@@ -10,7 +10,7 @@
 int main(void)
 {
     int array[] = {
-	    0, 1, 2, 5, 5, 6, 6, 7, 8, 9
+	    -5, -5, 0, 0, 0, 1, 1, 2, 2, 2, 5, 5, 6, 6, 6, 6, 7, 8, 9
     };
     size_t size = sizeof(array) / sizeof(array[0]);
 
@@ -20,5 +20,10 @@ int main(void)
     printf("Found %d at index: %d\n\n", 9, advanced_binary(array, size, 9));
     printf("Found %d at index: %d\n\n", 0, advanced_binary(array, size, 0));
     printf("Found %d at index: %d\n", 1, advanced_binary(array, size, 1));
+    printf("Found %d at index: %d\n", -5, advanced_binary(array, size, -5));
+    printf("Found %d at index: %d\n", 2, advanced_binary(array, size, 2));
+    printf("Found %d at index: %d\n", 6, advanced_binary(array, size, 6));
+    printf("Found %d at index: %d\n", 7, advanced_binary(array, size, 7));
+
     return (EXIT_SUCCESS);
 }
